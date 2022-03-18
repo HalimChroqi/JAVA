f0530b558d975d98d76afdc3d24871f1caaa369d
HalimChroqi <96173295+HalimChroqi@users.noreply.github.com>
2022-03-19 00:31:15 +0100

    TP4

---
+++ src/ma/education/tp/abstarctkeyword/Triangle.java
@@ -0,0 +1,11 @@
+package ma.education.tp.abstarctkeyword;
+
+public abstract class Triangle extends FormeGeoTypeA{
+    int h;
+    int l ;
+    @Override
+    public double calculerSurface() {
+
+        return (h*l)/2;
+    }
+}
---
+++ src/ma/education/tp/annotations/Calculatrice.java
@@ -0,0 +1,9 @@
+package ma.education.tp.annotations;
+
+import java.lang.annotation.Inherited;
+
+@Programmer(id=99,name="Samid karim")
+//@Inherited(id=10,name="Samir Amir")
+//@Inherited ne connecte pas les valeurs de l’annotation en utilisant la réflection.
+public class Calculatrice {
+}
